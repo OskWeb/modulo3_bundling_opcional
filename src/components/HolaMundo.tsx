1a46057e59066205636707fd25c3c8b071dd09c1
Oscar <oscararnu18@gmail.com>
2024-07-24 19:35:08 +0200

    prueba prettier con husky

--- src/components/HolaMundo.tsx
+++ src/components/HolaMundo.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import '../mystyles.css';
 
-                        export const HolaMundo = () => {
-                const textReact = 'Hola Mundo';
+export const HolaMundo = () => {
+  const textReact = 'Hola Mundo';
 
-  
-  
-                        return <span id="text-style">{textReact}</span>;
-        };
+  return <span id="text-style">{textReact}</span>;
+};
